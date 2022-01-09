0b9af8be96f38d54aa9b558ccee161514ce5a73b
Seamus McGill <seamusmcgill@gmail.com>
2022-01-09 18:32:03 -0500

    Changed category-1 to category-x in templates This avoid miscounts in data submission Added a function to add a new value to category Updated collectChartData to handle multiple category values Added event handlers to add category values Updated drawBarChart to find largest value

--- bar-chart-application.js
+++ bar-chart-application.js
@@ -1,8 +1,6 @@
 // Create a variable to store the number of categories
 let cat = 1;
 
-
-
 // Function to add form categories
 function addCategory() {
   // Use the template for new category section to create a new variable
@@ -13,21 +11,50 @@ function addCategory() {
   $(".category-" + cat + " #add-category").appendTo(".category-" + (cat + 1) + " .form-column:first");
   // Loop through each label in the category form and update the "for" to the next category number
   $(".category-" + (cat + 1) + " label").each(function(index) {
-    $(this).attr("for", ($(this).attr("for").replace("category-1", "category-" + (cat + 1))));
+    $(this).attr("for", ($(this).attr("for").replace("category-x", "category-" + (cat + 1))));
   })
-  // Loop through each input and update the ID to the next category number
+  // Loop through each input and update the associated ID/class to the next category number
   $(".category-" + (cat + 1) + " input").each(function(index) {
-    $(this).attr("id", ($(this).attr("id").replace("category-1", "category-" + (cat + 1))));
+    if ($(this).attr("id")) {
+      $(this).attr("id", ($(this).attr("id").replace("category-x", "category-" + (cat + 1))));
+    } else {
+      $(this).attr("class", ($(this).attr("class").replace("category-x", "category-" + (cat + 1))));
+    }
   })
   //Update the add category value button class to the next category number
-  $(".category-" + (cat + 1) + " button[class*='category-1']").attr("class",
-  $(".category-" + (cat + 1) + " button[class*='category-1']").attr("class").replace("category-1", "category-" + (cat + 1)));
+  $(".category-" + (cat + 1) + " button[class*='category-x']").attr("class",
+  $(".category-" + (cat + 1) + " button[class*='category-x']").attr("class").replace("category-x", "category-" + (cat + 1)));
 
   cat++;
+}
 
+// Function to add a value in the category
+function addCategoryValue() {
+  // Create empty variable to store current category
+  let currentCat = "";
+  // Extract the category number of the add value button just clicked
+  if ($(this).attr("class") === "add-category-1-value category-1") {
+    currentCat = $(this).attr("class").slice(30);
+  } else {
+    currentCat = $(this).attr("class").slice(28);
+  }
+
+  // Create a new category value input section for selected category
+  let newCatValue = "<br> <div class='category-" + currentCat + "-value-input'>" +$(".category-value-copy").html() + "</div>";
+  // Insert after the button just pressed
+  $(this).after(newCatValue);
+  // Loop through each label and update the "for" to the current category
+  $(".category-" + currentCat + "-value-input label").each(function(index) {
+    $(this).attr("for", ($(this).attr("for").replace("category-x", "category-" + currentCat)));
+  })
+  // Loop through each input and update the "class" to the current category
+  $(".category-" + currentCat + "-value-input input").each(function(index) {
+    $(this).attr("class", ($(this).attr("class").replace("category-x", "category-" + currentCat)));
+  })
 }
 
 function collectAppearanceData() {
+  // Collect all the appearance data from the first half of the chart
   let options = {
     title: $("#chart-title").val(),
     font: $("#title-font").val(),
@@ -42,32 +69,51 @@ function collectAppearanceData() {
 }
 
 function collectChartData() {
-  let data = [];
+  let userData = [];
   for (let i = 1; i <= cat; i++) {
+    // Grab category details from user input form
     let category = {
       name: $("#category-" + i + "-name").val(),
       color: $("#category-" + i + "-label-color").val(),
-      value: $("#category-" + i + "-value").val(),
-      valueColor: $("#category-" + i + "-value-color").val()
+      valueColor: $(".category-" + i + "-value-color").val()
     }
-    data.push(category);
+    // Check if there was more than one value entered for the category
+    if ($(".category-" + i + "-value").length > 1) {
+      // Create an array for the category values if so
+      category.value = [];
+      // Loop through all the data values
+      $(".category-" + i + "-value").each(function() {
+        category.value.push(parseInt($(this).val()));
+      })
+    } else {
+      category.value = parseInt($(".category-" + i + "-value").val());
+    }
+    // Push into data array
+    userData.push(category);
   }
-  console.log(data);
+  console.log(userData);
+  return userData;
 }
 
 function drawBarChart(data, options, element) {
   let largestValue = 0;
-  for (let i = 1; i <= cat; i++) {
-    if (data["category" + i + "Value"] > largestValue) {
-      largestValue = data["category" + i + "Value"];
+  for (let i = 0; i < cat; i++) {
+    if (Array.isArray(data[i]["value"])) {
+      for (let j = 0; j < data[i]["value"].length; j++) {
+        if (data[i]["value"][j] > largestValue) {
+          largestValue = data[i]["value"][j];
+        }
+      }
     }
+    else if (data[i]["value"] > largestValue) {
+      largestValue = data[i]["value"];
+    }
+    console.log("Pass " + i  + " the largest value is: " + largestValue);
   }
-
 }
 
 // jQuery code to run functions on event handler instances
 $(document).ready(function() {
-  $
   // Hide bar chart, title and x-axis and labels on load
   $(".chart-title").hide();
   $(".chart-container").hide();
@@ -75,7 +121,14 @@ $(document).ready(function() {
   $(".x-axis-label").hide();
   // Add category on button click
   $("#add-category").click(addCategory);
+  // Add category value when button clicked
+  // For adding values to the first form that is already on the page
+  $(".add-category-1-value").click(addCategoryValue);
+  // For adding values to dynamically generated categories
+  $("div").on("click", ".add-category-value", addCategoryValue);
   // Collect user inputted data on submit
   $("#submit").click(collectAppearanceData);
-  $("#submit").click(collectChartData);
+  $("#submit").on("click", function() {
+    drawBarChart(collectChartData());
+  });
 });
